375db396ceba5f6a89f9a6a56140df462e7dab17
azad12614 <azad.jishan2003@gmail.com>
2024-04-05 19:32:24 +0600

    teacher's form update

--- src/pages/Add/T_Form.jsx
+++ src/pages/Add/T_Form.jsx
@@ -12,6 +12,9 @@ function M_Form() {
     const phd = event.target.phd.value;
     const msc = event.target.msc.value;
     const bsc = event.target.bsc.value;
+    const gs = event.target.gs.value;
+    const rg = event.target.rg.value;
+    const od = event.target.od.value;
     const image = event.target.image.value;
     const sir = {
       name,
@@ -21,6 +24,9 @@ function M_Form() {
       phd,
       msc,
       bsc,
+      gs,
+      rg,
+      od,
       image,
     };
     fetch("https://cloud-xmqm.onrender.com/add-sir", {
@@ -161,6 +167,54 @@ function M_Form() {
               className="peer-focus:font-medium absolute text-md text-[#B6BBC4] dark:text-[#B6BBC4] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#B6BBC4] peer-focus:dark:text-[#B6BBC4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Role
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-3 group">
+            <input
+              type="text"
+              name="gs"
+              id="gs"
+              className="font-medium block py-2.5 px-0 w-full text-md bg-transparent border-b-2 border-[#B6BBC4] appearance-none focus:border-[#B6BBC4] focus:outline-none focus:ring-0 peer"
+              placeholder=" "
+              required=""
+            />
+            <label
+              htmlFor="gs"
+              className="peer-focus:font-medium absolute text-md text-[#B6BBC4] dark:text-[#B6BBC4] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#B6BBC4] peer-focus:dark:text-[#B6BBC4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Google Scholar
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-3 group">
+            <input
+              type="text"
+              name="rg"
+              id="rg"
+              className="font-medium block py-2.5 px-0 w-full text-md bg-transparent border-b-2 border-[#B6BBC4] appearance-none focus:border-[#B6BBC4] focus:outline-none focus:ring-0 peer"
+              placeholder=" "
+              required=""
+            />
+            <label
+              htmlFor="rg"
+              className="peer-focus:font-medium absolute text-md text-[#B6BBC4] dark:text-[#B6BBC4] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#B6BBC4] peer-focus:dark:text-[#B6BBC4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Research Gate
+            </label>
+          </div>
+          <div className="relative z-0 w-full mb-3 group">
+            <input
+              type="text"
+              name="od"
+              id="od"
+              className="font-medium block py-2.5 px-0 w-full text-md bg-transparent border-b-2 border-[#B6BBC4] appearance-none focus:border-[#B6BBC4] focus:outline-none focus:ring-0 peer"
+              placeholder=" "
+              required=""
+            />
+            <label
+              htmlFor="od"
+              className="peer-focus:font-medium absolute text-md text-[#B6BBC4] dark:text-[#B6BBC4] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#B6BBC4] peer-focus:dark:text-[#B6BBC4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              ORCID
             </label>
           </div>
           <div className="relative z-0 w-full mb-3 group">
--- src/pages/Update/T_Form.jsx
+++ src/pages/Update/T_Form.jsx
@@ -13,8 +13,11 @@ function Form() {
         const phd = event.target.phd.value;
         const msc = event.target.msc.value;
         const bsc = event.target.bsc.value;
+        const gs = event.target.gs.value;
+        const rg = event.target.rg.value;
+        const od = event.target.od.value;
         const image = event.target.image.value;
-        const Teacher = {
+        const sir = {
             name,
             email,
             number,
@@ -22,6 +25,9 @@ function Form() {
             phd,
             msc,
             bsc,
+            gs,
+            rg,
+            od,
             image,
         };
         fetch(`https://cloud-xmqm.onrender.com/update-sir/${sir?._id}`, {
@@ -164,6 +170,57 @@ function Form() {
                             className="peer-focus:font-medium absolute text-md text-[#B6BBC4] dark:text-[#B6BBC4] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#B6BBC4] peer-focus:dark:text-[#B6BBC4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                         >
                             Role
+                        </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-3 group">
+                        <input
+                            type="text"
+                            name="gs"
+                            id="gs"
+                            className="font-medium block py-2.5 px-0 w-full text-md bg-transparent border-b-2 border-[#B6BBC4] appearance-none focus:border-[#B6BBC4] focus:outline-none focus:ring-0 peer"
+                            placeholder=" "
+                            required=""
+                            defaultValue={sir?.gs}
+                        />
+                        <label
+                            htmlFor="gs"
+                            className="peer-focus:font-medium absolute text-md text-[#B6BBC4] dark:text-[#B6BBC4] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#B6BBC4] peer-focus:dark:text-[#B6BBC4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            Google Scholar
+                        </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-3 group">
+                        <input
+                            type="text"
+                            name="rg"
+                            id="rg"
+                            className="font-medium block py-2.5 px-0 w-full text-md bg-transparent border-b-2 border-[#B6BBC4] appearance-none focus:border-[#B6BBC4] focus:outline-none focus:ring-0 peer"
+                            placeholder=" "
+                            required=""
+                            defaultValue={sir?.rg}
+                        />
+                        <label
+                            htmlFor="rg"
+                            className="peer-focus:font-medium absolute text-md text-[#B6BBC4] dark:text-[#B6BBC4] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#B6BBC4] peer-focus:dark:text-[#B6BBC4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            Research Gate
+                        </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-3 group">
+                        <input
+                            type="text"
+                            name="od"
+                            id="od"
+                            className="font-medium block py-2.5 px-0 w-full text-md bg-transparent border-b-2 border-[#B6BBC4] appearance-none focus:border-[#B6BBC4] focus:outline-none focus:ring-0 peer"
+                            placeholder=" "
+                            required=""
+                            defaultValue={sir?.od}
+                        />
+                        <label
+                            htmlFor="od"
+                            className="peer-focus:font-medium absolute text-md text-[#B6BBC4] dark:text-[#B6BBC4] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#B6BBC4] peer-focus:dark:text-[#B6BBC4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            ORCID
                         </label>
                     </div>
                     <div className="relative z-0 w-full mb-3 group">
