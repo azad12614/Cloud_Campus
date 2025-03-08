62f24544f2996dad7c8614f71e56d1cb98ff7359
azad12614 <azad.jishan2003@gmail.com>
2025-03-09 00:25:58 +0600

    Api Update

--- API/index.js
+++ API/index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 
 const url = `${process.env.DB_URL}`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(url, {
   serverApi: {
     version: ServerApiVersion.v1,
