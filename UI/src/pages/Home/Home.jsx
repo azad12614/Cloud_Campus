d78b904002de9433f62687abfc6db63164190f97
azad12614 <azad.jishan2003@gmail.com>
2025-03-09 15:25:48 +0600

    Cmt dlt

--- UI/src/pages/Home/Home.jsx
+++ UI/src/pages/Home/Home.jsx
@@ -10,32 +10,6 @@ function Heros() {
   return (
     <>
       <Navbar />
-      {/* <section class="section-hero">
-        <div class="heros">
-          <div class="hero-text-box">
-            <h1 class="heading-primary">Welcome to The CSE!</h1>
-            <p class="hero-description">
-              Embark on your journey to shape the digital world at the
-              International Islamic University Chittagong's Department of
-              Computer Science and Engineering. Here, you'll gain the knowledge
-              and skills to solve real-world problems, guided by Islamic
-              principles of ethics and excellence. Join our vibrant community
-              and become a leader in the ever-evolving field of technology.
-            </p>
-            <a
-              href="https://www.iiuc.ac.bd/home/apply-online"
-              target="_blank"
-              class="enroll"
-            >
-              Enroll Now
-            </a>
-          </div>
-          <div class="hero-img-box">
-            <img src={IIUC_CSE} alt="IIUC LOGO" className="hero-img" />
-          </div>
-        </div>
-      </section> */}
-
       <section className="hero-section">
         <div className="hero-content">
           {/* Logo and Title */}
